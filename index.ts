921f9745d3344689354e8b2a0c091b6cbc49e389
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-15 15:26:50 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,6 @@
+// Q87
+// this function extract first 10 character of a string by substring
+function extractcharacter(str : string) : string{
+    return str.substring(0,10);
+}
+console.log(extractcharacter("hello! coders "));
